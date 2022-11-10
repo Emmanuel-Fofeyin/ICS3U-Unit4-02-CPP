f4b6330e8792334d2c7399799fac5c1c65e40bf1
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-10 16:30:16 +0000

    added code

---
+++ ICS3U-Unit4-02.cpp
@@ -0,0 +1,44 @@
+//!/ usr / bin / env python3
+
+//Created by : Emmanuel
+//Created on : Nov 2022
+//This program uses a while loop and multiplies
+
+#include <iostream>
+#include <string>
+
+int positiveInteger;
+int loopCounter = 0;
+int multiplyInt = 1;
+
+int main() {
+    // This function finds factorial of a number using while loop
+
+    // Input
+
+    std::string positiveString;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> positiveString;
+    std::cout << std::endl;
+
+    // Process and output
+    try {
+        positiveInteger = std::stoi(positiveString);
+        if (positiveInteger >= 0) {
+            do {
+                loopCounter = loopCounter + 1;
+                multiplyInt = multiplyInt * loopCounter;
+            } while (loopCounter < positiveInteger);
+            if (loopCounter >= positiveInteger)
+                std::cout << "" << positiveInteger << "!"
+                          << " = " << multiplyInt << "" << std::endl;
+        } else {
+            std::cout << "You did not enter a positive integer." << std::endl;
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "" << positiveString
+                  << " is not a valid input." << std::endl;
+    }
+
+    std::cout << "\nDone.";
+}
